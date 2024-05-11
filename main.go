f211e76981d5afb3311cc4436932863e2ae44d93
Soham041201 <sattigeri.soham@gmail.com>
2024-05-11 08:44:53 +0530

    initial commit

---
+++ main.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
+)
+
+func main() {
+
+	host := "postgres"
+	port := 5432
+	user := "postgres"
+	password := "docker"
+	dbname := "postgres"
+
+	connectionString := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	db, err := sql.Open("postgres", connectionString)
+
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	if err := db.Ping(); err != nil {
+		log.Fatal(err)
+	} else {
+		log.Println("Successfully Connected")
+	}
+	router := gin.Default()
+	router.Use(dbMiddleware(db))
+	router.GET("/albums", getAlbumbs)
+	router.POST("/album", createAlbum)
+	router.Run(":8080")
+}
+
+func dbMiddleware(db *sql.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Set("db", db)
+		c.Next()
+	}
+}
+
+type album struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
+}
+
+var albums = []album{
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+func getAlbumbs(c *gin.Context) {
+	db := c.MustGet("db").(*sql.DB)
+	rows, _ := db.Query("SELECT * FROM albums")
+	results := []album{}
+
+	for rows.Next() {
+		var id string
+		var title string
+		var artist string
+		var price float64
+
+		err := rows.Scan(&id, &title, &artist, &price)
+		if err != nil {
+			panic(err)
+		}
+		results = append(results, album{id, title, artist, price})
+	}
+
+	c.IndentedJSON(http.StatusOK, results)
+}
+
+func createAlbum(c *gin.Context) {
+	var newAlbum album
+	db := c.MustGet("db").(*sql.DB)
+
+	_, dbErr := db.Exec(`CREATE TABLE IF NOT EXISTS albums (
+		id VARCHAR(255) PRIMARY KEY,
+		title TEXT NOT NULL,
+		artist TEXT NOT NULL,
+		price DECIMAL(10,2) NOT NULL
+	  )`)
+
+	if dbErr != nil {
+		c.AbortWithError(http.StatusInternalServerError, dbErr)
+		return
+	}
+
+	insertQuery := `INSERT INTO albums(id,title,artist,price) VALUES ($1,$2,$3,$4)`
+
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+	albums = append(albums, newAlbum)
+
+	_, err := db.Exec(insertQuery, newAlbum.ID, newAlbum.Title, newAlbum.Artist, newAlbum.Price)
+
+	if err != nil {
+		fmt.Print(err)
+		c.IndentedJSON(http.StatusInternalServerError, err)
+	}
+}
